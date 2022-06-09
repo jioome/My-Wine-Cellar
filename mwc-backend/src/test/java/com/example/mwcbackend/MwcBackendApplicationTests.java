c91ca4ca8872671a180857d57e8205f68ad3c0a7
Jioo Jung <jioome17@gmail.com>
2022-06-09 12:52:06 +0900

    Feat: Backend 초기설정

---
+++ mwc-backend/src/test/java/com/example/mwcbackend/MwcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mwcbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MwcBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
