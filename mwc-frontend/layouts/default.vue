a26821f70e895cc41f66f638889ffe894fc18750
jwun95 <jwun95@naver.com>
2022-06-13 14:32:46 +0900

    Feat: Main Page 기본 레이아웃 구현

--- mwc-frontend/layouts/default.vue
+++ mwc-frontend/layouts/default.vue
@@ -1,60 +1,39 @@
 <template>
-  <div>
-    <nav
-      class="navbar header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div class="navbar-brand">
-        <a class="navbar-item" href="/">
-          <img src="~assets/buefy.png" alt="Buefy" height="28" />
-        </a>
-
-        <div class="navbar-burger">
-          <span />
-          <span />
-          <span />
-        </div>
-      </div>
-    </nav>
-
-    <section class="main-content columns">
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">General</p>
-        <ul class="menu-list">
-          <li v-for="(item, key) of items" :key="key">
-            <NuxtLink :to="item.to" exact-active-class="is-active">
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </NuxtLink>
-          </li>
-        </ul>
-      </aside>
-
-      <div class="container column is-10">
-        <Nuxt />
-      </div>
-    </section>
+  <div class="mobile-layout">
+    <main>
+      <Nuxt />
+    </main>
+    <div class="mobile-navbar">
+      <Navbar />
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'DefaultLayout',
-  data() {
-    return {
-      items: [
-        {
-          title: 'Home',
-          icon: 'home',
-          to: { name: 'index' },
-        },
-        {
-          title: 'Inspire',
-          icon: 'lightbulb',
-          to: { name: 'inspire' },
-        },
-      ],
-    }
-  },
-}
+  export default {
+    name: 'MobileLayout'
+  }
 </script>
+
+<style lang="scss" scoped>
+.mobile-layout {
+  max-width: 768px;
+  margin: auto;
+  min-height: 100vh;
+  background: #a0a0a0;
+}
+main {
+  height: 90vh;
+}
+.mobile-navbar {
+  width: 414px;
+  margin: auto;
+  display: flex;
+  align-items: center;
+  background: #fff;
+  height: 10vh;
+  position: fixed;
+  bottom: 0;
+  z-index: 1000;
+}
+</style>
